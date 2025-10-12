b63b8bd471d6604ffcadaf8c6e303ee0920aef00
Pritish Mishra <pritishmishra579@gmail.com>
2025-10-12 13:38:13 +0530

    add WebSocket endpoint for real-time click tracking

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,4 +1,4 @@
-import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
+import { captureLinkClickInBackground, getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 import { Hono } from 'hono';
@@ -33,7 +33,20 @@ App.get('/:id', async (c) => {
 			timestamp: new Date().toISOString(),
 		},
 	};
-	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
+	c.executionCtx.waitUntil(captureLinkClickInBackground(c.env, queueMessage));
 
 	return c.redirect(destination);
+});
+
+App.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const accountId = c.req.header('account-id');
+	if (!accountId) return c.text('No Headers', 404);
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
 });
